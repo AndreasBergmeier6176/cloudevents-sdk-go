1521bc6b348d32e4dddff43ecd1d8025eb6f24de
Andreas Bergmeier <andreas.bergmeier@otto.de>
2024-04-05 16:00:18 +0200

    Ensure that editing fetched Attributes is race-free in any case.

--- protocol/pubsub/v2/attributes.go
+++ protocol/pubsub/v2/attributes.go
@@ -14,7 +14,20 @@ import (
 type withCustomAttributes struct{}
 
 func AttributesFrom(ctx context.Context) map[string]string {
-	return binding.GetOrDefaultFromCtx(ctx, withCustomAttributes{}, make(map[string]string)).(map[string]string)
+	ctxVal := binding.GetOrDefaultFromCtx(ctx, withCustomAttributes{}, nil)
+	if ctxVal == nil {
+		return make(map[string]string, 0)
+	}
+
+	m := ctxVal.(map[string]string)
+
+	// Since it is possible that we get the same map from one ctx multiple times
+	// we need to make sure, that it is race free to modify said map.
+	cp := make(map[string]string, len(m))
+	for k, v := range m {
+		cp[k] = v
+	}
+	return cp
 }
 
 // WithCustomAttributes sets Message Attributes without any CloudEvent logic.
